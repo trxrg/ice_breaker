38f3cf27b167385d66160e40d35f8feab1756ee5
trxrg <tarikrg@gmail.com>
2025-05-19 14:26:00 +0300

    add linkedin.py

---
+++ third_parties/linkedin.py
@@ -0,0 +1,11 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+    """scrape information from LinkedIn profiles,
+    Manually scrape the information from the LInkedIn profile"""
+    
